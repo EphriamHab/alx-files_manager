39c5784d789ab95dfcc9eb0aaac1f399124a85bb
EphriamHab <ephremhabtamu2015@gmail.com>
2023-11-30 17:11:47 +0300

    install package and dependecy

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        browser: false,
+        es6: true,
+        jest: false,
+        mocha: true,
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
